3fe09312804d9d667e836c39e2d0b86b1811099a
Stuart Kuredjian <108@holbertonschool.com>
2017-10-06 21:35:35 +0000

    modularize function

--- apple_api/get_images.js
+++ apple_api/get_images.js
@@ -1,32 +1,21 @@
-artistNames = ['Jack Johnson', 'Cold play', 'Taylor Swift']
+//artistNames = ['Jack Johnson', 'Cold play', 'Taylor Swift']
 retDict = {};
 artistBase = 'https://itunes.apple.com/ca/artist/';
+imgDict = {};
 
-getImage('fhdjska');
+//getitunesArtistImages(['Taylor Swift', 'Green Day', 'Michael Jackson']);
+function getitunesArtistImages (artistNames) {
+  for (let i=0; i < artistNames.length; i++) {
+    const name = artistNames[i];
+    getId(name);
+  }
 
-for (let i=0; i < artistNames.length; i++) {
-  const name = artistNames[i];
-  getId(name);
+  for (name in retDict) {
+    getImage(name, retDict[name]);
+  }
+  console.log(imgDict);
+  return imgDict;
 }
-
-const testId = retDict['Jack Johnson'];
-
-getImage(retDict['Jack Johnson']);
-
-for (name in retDict) {
-//  console.log(retDict[name])
-}
-
-
-/*
-  $.get ('https://itunes.apple.com/search\?term\=' + artistNames[i], function (data) {
-    const results = JSON.parse(data).results;
-    const artistId = results;
-    retDict[name] = results[i].artistId;
-    console.log(retDict);
-
-  });
-*/
 function getId (name) {
   $.ajax({
     async: false, 
@@ -40,27 +29,21 @@ function getId (name) {
   });
 }
 
-  console.log(retDict);
-
-function getImage (artistId) {
-
+function getImage (artist, artistId) {
+  console.log(artist);
+  console.log(artistId);
   $.ajax({
     async: false, 
-    url:  'https://itunes.apple.com/ca/artist/909253',
-//    url: 'artistBase' + retDict[artistId],
+    url:  'https://itunes.apple.com/ca/artist/' + artistId,
     type: 'GET',
     success: function (res) {
-       imgURL = res.match('<meta content=\"[a-zA-Z0-9 :\/\.\-]+.jpg\" property=\"og:image\" \/>')[0];
-       console.log(imgURL);
+       const regex = 'meta property=';
+       imgURL = res.match('<meta property="og:image" content="([a-zA-Z0-9 :\/\.\-]+.jpg)" id="ember[0-9]+" class="ember-view">')[1];
+       imgDict[name] = imgURL;
+       
     },
     error: function (res) {
       console.log('ERROR!!!!');
     }
   });
 }
-//}
-
-
-//<meta property="og:image" content="https://is3-ssl.mzstatic.com/image/thumb/Music128/v4/5f/9e/f0/5f9ef07f-a666-cf03-7c84-c858dce2eb40/source/1200x630sr.jpg" id="ember13122855" class="ember-view">
-
-// https?:\/\/?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)
