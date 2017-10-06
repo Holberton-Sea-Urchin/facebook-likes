5856afbcb1c2ca4378a349909425d712c82cfa32
team-sea-urchin <ubuntu@fb-media-1.local>
2017-10-06 21:22:40 +0000

    updated javascript with music response from KIM and KEVIN changes

--- html/js/facebook-oauth.js
+++ html/js/facebook-oauth.js
@@ -41,26 +41,31 @@ function Login()
     {
       console.log('User cancelled login or did not fully authorize.');
     }
-  },{scope: 'email,user_photos,user_videos'});
+  },{scope: 'email,user_likes,user_videos'});
 }
 function getUserInfo() {
   FB.api('/me', function(response) {
 
     var str="<b>Name</b> : "+response.name+"<br>";
-    str +="<b>Link: </b>"+response.link+"<br>";
-    str +="<b>Username:</b> "+response.username+"<br>";
-    str +="<b>id: </b>"+response.id+"<br>";
-    str +="<b>Email:</b> "+response.email+"<br>";
-    str +="<input type='button' value='Get Photo' onclick='getPhoto();'/>";
+    //str +="<b>Link: </b>"+response.link+"<br>";
+    //str +="<b>Username:</b> "+response.username+"<br>";
+    //str +="<b>id: </b>"+response.id+"<br>";
+    //str +="<b>Email:</b> "+response.email+"<br>";
+    str +="<input type='button' value='Get Music' onclick='getMusic();'/>";
     str +="<input type='button' value='Logout' onclick='Logout();'/>";
     document.getElementById("status").innerHTML=str;
 
   });
 }
-function getPhoto()
-{
-  FB.api('/me/picture?type=normal', function(response) {
 
+let musicList = [];
+function getMusic()
+{
+  FB.api('/me/music', function(response) {
+	for (let i = 0; i < response['data'].length; i++) {
+			musicList.push(response['data'][i]['name']);
+	}
+	console.log(musicList);
     var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
     document.getElementById("status").innerHTML+=str;
 
