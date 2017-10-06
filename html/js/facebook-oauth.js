4dfb0302547e51117f938be080a8d4873a8824b6
johncoleman83 <djc_fileno@davidjohncoleman.com>
2017-10-06 14:47:27 -0700

    added Stuart and Meghas API get request to Apple

--- html/js/facebook-oauth.js
+++ html/js/facebook-oauth.js
@@ -62,12 +62,12 @@ let musicList = [];
 function getMusic()
 {
   FB.api('/me/music', function(response) {
-	for (let i = 0; i < response['data'].length; i++) {
-			musicList.push(response['data'][i]['name']);
-	}
-	console.log(musicList);
-    var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
-    document.getElementById("status").innerHTML+=str;
+    for (let i = 0; i < response['data'].length; i++) {
+      musicList.push(response['data'][i]['name']);
+    }
+    console.log(musicList);
+    let artistNames = getitunesArtistImages(musicList);
+    console.log(artistNames);
 
   });
 
@@ -85,3 +85,55 @@ function Logout()
   js.src = "//connect.facebook.net/en_US/all.js";
   ref.parentNode.insertBefore(js, ref);
 }(document));
+
+
+// APPLE API CHANGES
+
+//artistNames = ['Jack Johnson', 'Cold play', 'Taylor Swift']
+retDict = {};
+artistBase = 'https://itunes.apple.com/ca/artist/';
+imgDict = {};
+
+//getitunesArtistImages(['Taylor Swift', 'Green Day', 'Michael Jackson']);
+function getitunesArtistImages (artistNames) {
+  for (let i=0; i < artistNames.length; i++) {
+    const name = artistNames[i];
+    getId(name);
+  }
+
+  for (name in retDict) {
+    getImage(name, retDict[name]);
+  }
+  console.log(imgDict);
+  return imgDict;
+}
+function getId (name) {
+  $.ajax({
+    async: false,
+    url: 'https://itunes.apple.com/search\?term\=' + name,
+    type: 'GET',
+    contentType: 'application/json',
+    dataType: 'json',
+    success: function (res) {
+      retDict[name] = res.results[0].artistId;
+    }
+  });
+}
+
+function getImage (artist, artistId) {
+  console.log(artist);
+  console.log(artistId);
+  $.ajax({
+    async: false,
+    url:  'https://itunes.apple.com/ca/artist/' + artistId,
+    type: 'GET',
+    success: function (res) {
+      const regex = 'meta property=';
+      imgURL = res.match('<meta property="og:image" content="([a-zA-Z0-9 :\/\.\-]+.jpg)" id="ember[0-9]+" class="ember-view">')[1];
+      imgDict[name] = imgURL;
+    },
+    error: function (res) {
+      console.log('ERROR!!!!');
+    }
+  });
+}
